66b76a5746fb306f65de33a78ba00e9611217f3f
Samantha Dickeson <samlucydickeson@gmail.com>
2019-05-14 17:48:22 +0100

    add failure route Relates #131 #134 #135 #137

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,27 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const router = express.Router();
+const { failureMessage } = require("./failure-messages");
 
-router.get('/', (req, res) => {
-  res.render("home")
-    console.log("This is the home route.")
+router.get("/", (req, res) => {
+  res.render("home");
+  console.log("This is the home route.");
+});
+
+router.get("/dish-list-failure", (req, res) => {
+  failureMessage(res, "listing", "dish");
+});
+
+router.get("/dish-claim-failure", (req, res) => {
+  failureMessage(res, "claiming", "dish");
+});
+
+router.get("/community-add-failure", (req, res) => {
+  failureMessage(res, "adding", "community");
+});
+
+router.get("/community-join-failure", (req, res) => {
+  failureMessage(res, "joining", "community");
 });
 
 module.exports = router;
