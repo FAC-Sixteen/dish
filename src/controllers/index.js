5fb5ee9b9c533dad22ecbb3b90651f7645b99379
Ania Blaziak <anblaziak@gmail.com>
2019-05-14 15:10:26 +0100

    handle home route relates #112

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    console.log("This is the home route.")
+});
+
+module.exports = router;
