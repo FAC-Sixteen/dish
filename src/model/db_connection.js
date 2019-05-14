230c9eb262557e2a2ceae1f36f0f60a49fcca2d1
Samantha Dickeson <samlucydickeson@gmail.com>
2019-05-14 15:22:04 +0100

    write db_connection script

---
+++ src/model/db_connection.js
@@ -0,0 +1,14 @@
+const { Pool } = require("pg");
+require("dotenv")("./config.env");
+
+let connectionString =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
+if (!connectionString) throw new Error("Database url must be set");
+
+module.exports = new Pool({
+  connectionString,
+  ssl: !connectionString.includes("localhost")
+});
