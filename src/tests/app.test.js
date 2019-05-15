f3de9b1dc7af73c415d4ef8bb67e675322877fcc
dubhcait <dubhcait@live.ie>
2019-05-15 16:46:19 +0100

    add /login, /main /about route tests

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -81,4 +81,109 @@ describe('Test the community-add path', () => {
         })
     });
 })
+  
 
+describe('Test the dish-list-failure path', () => {
+    test('It should response the GET method', () => {
+        return request(app).get("/dish-list-failure").then(response => {
+            expect(response.statusCode).toBe(200)
+        })
+    });
+})
+
+describe('Test the dish-claim-failure path', () => {
+    test('It should response the GET method', () => {
+        return request(app).get("/dish-claim-failure").then(response => {
+            expect(response.statusCode).toBe(200)
+        })
+    });
+})
+
+describe('Test the community-add-failure path', () => {
+    test('It should response the GET method', () => {
+        return request(app).get("/community-add-failure").then(response => {
+            expect(response.statusCode).toBe(200)
+        })
+    });
+})
+
+describe('Test the community-join-failure path', () => {
+    test('It should response the GET method', () => {
+        return request(app).get("/community-join-failure").then(response => {
+            expect(response.statusCode).toBe(200)
+        })
+    });
+})
+  
+describe('Test the account-register-failure path', () => {
+    test('It should response the GET method', () => {
+        return request(app).get("/account-register-failure").then(response => {
+            expect(response.statusCode).toBe(200)
+        })
+    });
+})
+
+describe('Test the dish-list-success path', () => {
+    test('It should response the GET method', () => {
+        return request(app).get("/dish-list-success").then(response => {
+            expect(response.statusCode).toBe(200)
+        })
+    });
+})
+
+describe('Test the dish-claim-success path', () => {
+    test('It should response the GET method', () => {
+        return request(app).get("/dish-claim-success").then(response => {
+            expect(response.statusCode).toBe(200)
+        })
+    });
+})
+
+
+describe('Test the community-add-success path', () => {
+    test('It should response the GET method', () => {
+        return request(app).get("/community-add-success").then(response => {
+            expect(response.statusCode).toBe(200)
+        })
+    });
+})
+
+describe('Test the community-join-success path', () => {
+    test('It should response the GET method', () => {
+        return request(app).get("/community-join-success").then(response => {
+            expect(response.statusCode).toBe(200)
+        })
+    });
+})
+
+describe('Test the account-register-success path', () => {
+    test('It should response the GET method', () => {
+        return request(app).get("/account-register-success").then(response => {
+            expect(response.statusCode).toBe(200)
+        })
+    });
+})
+
+describe('Test the login path', () => {
+    test('It should response the GET method', () => {
+        return request(app).get("/login").then(response => {
+            expect(response.statusCode).toBe(200)
+        })
+    });
+})
+
+describe('Test the main path', () => {
+    test('It should response the GET method', () => {
+        return request(app).get("/main").then(response => {
+            expect(response.statusCode).toBe(200)
+        })
+    });
+})
+
+describe('Test the about path', () => {
+    test('It should response the GET method', () => {
+        return request(app).get("/about").then(response => {
+            expect(response.statusCode).toBe(200)
+        })
+    });
+})
