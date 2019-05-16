5c9e5c675dac32f5b3300f658281b91aa635616e
Ania Blaziak <anblaziak@gmail.com>
2019-05-16 11:23:17 +0100

    populate each listing with data

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -13,21 +13,48 @@ router.get("/", (req, res) => {
 // Success/failure pages routes
 
 router.get("/:item-:action-:outcome", (req, res) => {
-  const { item, action, outcome } = req.params;
-  res.render(outcome, { action, item });
+  const {
+    item,
+    action,
+    outcome
+  } = req.params;
+  res.render(outcome, {
+    action,
+    item
+  });
 });
 
 // Listings pages routes
 
 router.get("/:item-:type", (req, res) => {
-  const { item, type } = req.params;
-  res.render(type, { type, item });
+  const {
+    item,
+    type
+  } = req.params;
+  const data = [{
+      image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-breakfast-burrito-horizontaljpg-1541624805.jpg?resize=980:*',
+      teaser: 'THIS IS A TEST TEASER.',
+      name: 'Burrito'
+    },
+    {
+      image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-breakfast-burrito-horizontaljpg-1541624805.jpg?resize=980:*',
+      teaser: 'This is a better burrito.',
+      name: 'Better Burrito'
+    }
+  ]
+  res.render(type, {
+    type,
+    item,
+    data
+  });
 });
 
 // Basic pages routes
 
 router.get("/:path", (req, res) => {
-  const { path } = req.params;
+  const {
+    path
+  } = req.params;
   res.render(path);
 });
 
