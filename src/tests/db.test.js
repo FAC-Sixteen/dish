3be452ac60c6a5d0eb52a153f9b7bb73ae7667b7
Samantha Dickeson <samlucydickeson@gmail.com>
2019-05-21 16:01:07 +0100

    try db.end

--- src/tests/db.test.js
+++ src/tests/db.test.js
@@ -4,19 +4,19 @@ const db = require("../model/db_connection");
 const build = require("../model/db_build");
 const { getDishListings, getSpecificDish } = require("../queries/getDishData");
 
-beforeEach(async () => {
-  await build();
-});
-
-afterEach(async done => {
-  await db.close;
-  done();
-});
-
 describe("Database queries", () => {
+  beforeEach(async () => {
+    await build();
+  });
+
+  afterEach(async done => {
+    await db.end();
+    done();
+  });
+
   test("Returns all dishes", async () => {
     return getDishListings().then(data => {
-      expect(data).toBeTruthy;
+      return expect(data).toBeTruthy;
     });
   });
 });
