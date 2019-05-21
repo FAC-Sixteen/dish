078f3c11c3998efd144750190338848c2b816578
Samantha Dickeson <samlucydickeson@gmail.com>
2019-05-21 17:01:56 +0100

    write database tests

--- src/queries/getDishData.js
+++ src/queries/getDishData.js
@@ -1,21 +1,25 @@
 const db = require("../model/db_connection.js");
 
-const getDishListings = () => {
-  return db
-    .query("SELECT * FROM dishes")
-    .then(response => {
+const getDishListings = param => {
+  if (param !== undefined) {
+    throw Error("There was an argument");
+  } else {
+    return db.query("SELECT * FROM dishes").then(response => {
       return response.rows;
-    })
-    .catch(error => console.log(error));
+    });
+  }
 };
 
 const getSpecificDish = ID => {
-  return db
-    .query("SELECT * FROM dishes WHERE dishID = $1", [ID])
-    .then(response => {
-      return response.rows;
-    })
-    .catch(error => console.log(error));
+  if (typeof ID !== "number") {
+    throw Error("Invalid argument, should be an integer");
+  } else {
+    return db
+      .query("SELECT * FROM dishes WHERE dishID = $1", [ID])
+      .then(response => {
+        return response.rows;
+      });
+  }
 };
 
 module.exports = {
--- src/tests/db.test.js
+++ src/tests/db.test.js
@@ -3,20 +3,64 @@ require("dotenv").config();
 const db = require("../model/db_connection");
 const build = require("../model/db_build");
 const { getDishListings, getSpecificDish } = require("../queries/getDishData");
+const {
+  getCommunityListings,
+  getSpecificCommunity
+} = require("../queries/getCommunityData");
 
-describe("Database queries", () => {
-  beforeEach(async () => {
-    await build();
-  });
+beforeEach(async () => {
+  await build();
+});
 
-  afterEach(async done => {
-    await db.end();
-    done();
-  });
+afterAll(async done => {
+  await db.end();
+  done();
+});
 
-  test("Returns all dishes", async () => {
+describe("Database queries return data", () => {
+  test("getDishListings returns all dishes", async () => {
     return getDishListings().then(data => {
-      return expect(data).toBeTruthy;
+      expect(data).toBeTruthy();
     });
+  });
+  test("getSpecificDish returns a dish", async () => {
+    return getSpecificDish(1).then(data => {
+      expect(data).toBeTruthy();
+    });
+  });
+
+  test("getCommunityListings returns all communities", async () => {
+    return getCommunityListings().then(data => {
+      expect(data).toBeTruthy();
+    });
+  });
+  test("getSpecificCommunity returns a community", async () => {
+    return getSpecificCommunity(1).then(data => {
+      expect(data).toBeTruthy();
+    });
+  });
+});
+
+describe("Database queries catch errors", () => {
+  test("getDishListings throws an error if called with an argument", async () => {
+    expect(() => {
+      getDishListings("octopus");
+    }).toThrow();
+  });
+  test("getSpecificDish throws an error if not given an integer", async () => {
+    expect(() => {
+      getSpecificDish("octopus");
+    }).toThrow();
+  });
+
+  test("getCommunityListings throws an error if called with an argument", async () => {
+    expect(() => {
+      getCommunityListings("octopus");
+    }).toThrow();
+  });
+  test("getSpecificCommunity throws an error if not given an integer", async () => {
+    expect(() => {
+      getSpecificCommunity("octopus");
+    }).toThrow();
   });
 });
