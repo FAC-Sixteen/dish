339a911809d968a55a720165df3769dea8719fe8
Samantha Dickeson <samlucydickeson@gmail.com>
2019-05-23 14:51:26 +0100

    modify test for titlecase

--- src/tests/helpers.test.js
+++ src/tests/helpers.test.js
@@ -3,8 +3,8 @@ const helpers = require("../views/helpers");
 
 describe("Test the titlecase function", () => {
   test("It should output a string with the first letter upper case", () => {
-    const actual = helpers.titlecase("this is a test");
-    const expected = "This Is A Test";
+    const actual = helpers.titlecase("this is our test");
+    const expected = "This Is Our Test";
     expect(actual).toBe(expected);
   });
 });
